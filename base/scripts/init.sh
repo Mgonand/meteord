1b096d9c9caaf128d163fa2b0a3caa47c39ee0c8
Marcos González Andrés <marcos@binpar.com>
2016-04-08 20:33:48 +0200

    Update init.sh

--- base/scripts/init.sh
+++ base/scripts/init.sh
@@ -4,5 +4,4 @@ bash $METEORD_DIR/lib/install_base.sh
 bash $METEORD_DIR/lib/install_node.sh
 bash $METEORD_DIR/lib/install_phantomjs.sh
 bash $METEORD_DIR/lib/install_graphicsmagick.sh
-bash $METEORD_DIR/lib/install_ffmpeg.sh
 bash $METEORD_DIR/lib/cleanup.sh
